f0fe0e6d574ee930d58bcd521fde7c2f3c352263
Max7707 <makkir7@gmail.com>
2026-03-08 12:20:28 +0300

    mod task_e

--- binarysearch/task_e.py
+++ binarysearch/task_e.py
@@ -2,12 +2,12 @@ count_2 = int(input())
 count_3 = int(input())
 count_4 = int(input())
 
-r = 10**15
+r = 10**18
 l = 0
 
 while l < r:
     m = (r + l) // 2
-    if ((m * 5 + count_2 * 2 + count_3 * 3 + count_4 * 4) / (m + count_4 + count_2 + count_3)) >= 3.5:
+    if (m * 5 + count_2 * 2 + count_3 * 3 + count_4 * 4) * 2 >= 7 * (m + count_4 + count_2 + count_3):
         r = m
     else:
         l = m + 1
